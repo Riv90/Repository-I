5cd4cfdb9f53e091dcb8aae06737e108a2b60a17
Bartek Leszcz <leszczbartoszinfo@gmail.com>
2018-05-15 09:46:09 +0200

    mod3

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void random( int* ptr, int n, int m)
+void fillRandom( int* ptr, int n, int m)
 {
 	for (int i = 0; i < n*m; i++)
 	{
-		tab[i]= rand() % 50;
+		ptr[i]= rand() % 50;
 	}
 }
 
@@ -14,7 +15,7 @@ void print(int* ptr, int n, int m)
 	{
 		for(int j = 0; j < m; j++)
 		{
-			printf("%.3d",tab[j + i + n*m]);
+			printf("%.3d ",ptr[j + i * n]);
 		}
 		printf("\n");
 	}
@@ -24,7 +25,7 @@ int main()
 {
 	int n = 5;
 	int* ptr = calloc(n*n, sizeof(int));
-	random(ptr , n, n );
+	fillRandom(ptr , n, n );
 	print(ptr,n ,n);
 	return 0;
 }
